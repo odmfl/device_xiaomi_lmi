4c3c66250817993da55c12a302bb0eb6069799fa
¨raystef66¨ <s.vanbarel@gmail.com>
2024-04-03 17:02:46 +0200

    lmi: parts: update parts after QPR2 merge

--- parts/src/org/lineageos/settings/popupcamera/PopupCameraSettingsActivity.java
+++ parts/src/org/lineageos/settings/popupcamera/PopupCameraSettingsActivity.java
@@ -20,7 +20,6 @@ package org.lineageos.settings.popupcamera;
 import android.os.Bundle;
 
 import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
-import com.android.settingslib.widget.R;
 
 public class PopupCameraSettingsActivity extends CollapsingToolbarBaseActivity {
 
@@ -30,7 +29,7 @@ public class PopupCameraSettingsActivity extends CollapsingToolbarBaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        getFragmentManager().beginTransaction().replace(R.id.content_frame,
+        getFragmentManager().beginTransaction().replace(com.android.settingslib.collapsingtoolbar.R.id.content_frame,
                 new PopupCameraSettingsFragment(), TAG_POPUPCAMERA).commit();
     }
 }
